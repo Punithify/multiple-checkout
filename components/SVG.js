452d69f5cbfcfde6e345f1b74aa1330cd78e2924
Punith <punith@pop-os.localdomain>
2022-08-05 14:45:29 +0530

    fixed

--- components/SVG.js
+++ components/SVG.js
@@ -6,14 +6,14 @@ export default function SVG() {
       xmlns="http://www.w3.org/2000/svg"
       width="163"
       height="80.9"
-      class="mx-auto"
+      className="mx-auto"
       viewBox="-252.3 356.1 163 80.9"
-      enable-background="new -252.3 356.1 163 80.9"
+      enableBackground="new -252.3 356.1 163 80.9"
     >
       <path
         stroke="#B2B2B2"
-        stroke-width="2"
-        stroke-miterlimit="10"
+        strokeWidth="2"
+        strokeMiterlimit="10"
         d="M-108.9 404.1v30c0 1.1-.9 2-2 2h-120.1c-1.1 0-2-.9-2-2v-75c0-1.1.9-2 2-2h120.1c1.1 0 2 .9 2 2v37m-124.1-29h124.1"
         fill="none"
       ></path>
@@ -22,8 +22,8 @@ export default function SVG() {
       <circle fill="#B2B2B2" cx="-216.6" cy="361.9" r="1.8"></circle>
       <path
         stroke="#B2B2B2"
-        stroke-width="2"
-        stroke-miterlimit="10"
+        strokeWidth="2"
+        strokeMiterlimit="10"
         d="M-128.7 400.1h36.7m-3.6-4.1l4 4.1-4 4.1"
         fill="none"
       ></path>
